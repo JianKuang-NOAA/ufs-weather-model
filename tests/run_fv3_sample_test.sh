3f375aac12e52c0bb9d7f15e22016afa0b6e60c3
jian.kuang <Jian.Kuang@noaa.gov>
2020-11-06 23:11:37 -0600

    update script

--- tests/run_fv3_sample_test.sh
+++ tests/run_fv3_sample_test.sh
@@ -5,7 +5,7 @@
 
 set -eux
 
-export PATHRT=/work/noaa/marine/Jian.Kuang/ufs-weather-model
+export UFS_REPO=${PWD}/..
 export RUNDIR_ROOT=/work/noaa/marine/Jian.Kuang/dataroot/stmp/jkuang
 export TEST_NAME=fv3_ccpp_control
 # ? name
@@ -17,12 +17,12 @@ source sampletest_vars.sh
 source tests/$TEST_NAME
 
 export INPUT_DIR=${CNTL_DIR}
-export RUNDIR=${RUNDIR_ROOT}/${TEST_NAME}${RT_SUFFIX}
-export CNTL_DIR=${CNTL_DIR}${BL_SUFFIX}
+export RUNDIR=${RUNDIR_ROOT}/${TEST_NAME}
+export CNTL_DIR=${CNTL_DIR}
 
-export JBNME=$(basename $RUNDIR_ROOT)_${TEST_NR}
+export JBNME=$(basename $RUNDIR_ROOT)_${TEST_NAME}
 
-export REGRESSIONTEST_LOG=${LOG_DIR}/rt_${TEST_NR}_${TEST_NAME}${RT_SUFFIX}.log
+export TEST_LOG=${RUNDIR}/${TEST_NAME}.log
 
 # get rid of it
 #source rt_utils.sh
@@ -39,42 +39,42 @@ cd $RUNDIR
 ####################################
 
 # FV3 executable:
-cp ${PATHRT}/ufs_model .
+cp ${UFS_REPO}/build_$TEST_NAME/ufs_model .
 
 # modulefile for FV3:
-cp ${PATHRT}/modules.fv3_${COMPILE_NR} modules.fv3
+cp ${UFS_REPO}/modulefiles/${MACHINE_ID}/fv3 modules.fv3
 
 # module-setup.sh
-cp ${PATHRT}/module-setup.sh.inc module-setup.sh
+cp ${UFS_REPO}/NEMS/src/conf/module-setup.sh.inc module-setup.sh
 
-SRCD="${PATHTR}"
+SRCD="${UFS_REPO}"
 RUND="${RUNDIR}"
 
-atparse < ${PATHRT}/fv3_conf/${FV3_RUN:-fv3_run.IN} > fv3_run
+atparse < ${UFS_REPO}/tests/fv3_conf/${FV3_RUN:-fv3_run.IN} > fv3_run
 
-atparse < ${PATHRT}/parm/${INPUT_NML:-input.nml.IN} > input.nml
+atparse < ${UFS_REPO}/tests/parm/${INPUT_NML:-input.nml.IN} > input.nml
 
-atparse < ${PATHRT}/parm/${MODEL_CONFIGURE:-model_configure.IN} > model_configure
+atparse < ${UFS_REPO}/tests/parm/${MODEL_CONFIGURE:-model_configure.IN} > model_configure
 
-atparse < ${PATHRT}/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
+atparse < ${UFS_REPO}/tests/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
 
 source ./fv3_run
 
 if [[ $DATM = 'true' ]] || [[ $S2S = 'true' ]]; then
-  edit_ice_in     < ${PATHRT}/parm/ice_in_template > ice_in
-  edit_mom_input  < ${PATHRT}/parm/${MOM_INPUT:-MOM_input_template_$OCNRES} > INPUT/MOM_input
-  edit_diag_table < ${PATHRT}/parm/diag_table_template > diag_table
-  edit_data_table < ${PATHRT}/parm/data_table_template > data_table
+  edit_ice_in     < ${UFS_REPO}/tests/parm/ice_in_template > ice_in
+  edit_mom_input  < ${UFS_REPO}/tests/parm/${MOM_INPUT:-MOM_input_template_$OCNRES} > INPUT/MOM_input
+  edit_diag_table < ${UFS_REPO}/tests/parm/diag_table_template > diag_table
+  edit_data_table < ${UFS_REPO}/tests/parm/data_table_template > data_table
   # CMEPS
-  cp ${PATHRT}/parm/fd_nems.yaml fd_nems.yaml
-  cp ${PATHRT}/parm/pio_in pio_in
-  cp ${PATHRT}/parm/med_modelio.nml med_modelio.nml
+  cp ${UFS_REPO}/tests/parm/fd_nems.yaml fd_nems.yaml
+  cp ${UFS_REPO}/tests/parm/pio_in pio_in
+  cp ${UFS_REPO}/tests/parm/med_modelio.nml med_modelio.nml
 fi
 if [[ $DATM = 'true' ]]; then
-  cp ${PATHRT}/parm/datm_data_table.IN datm_data_table
+  cp ${UFS_REPO}/tests/parm/datm_data_table.IN datm_data_table
 fi
 
-atparse < ${PATHRT}/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
+atparse < ${UFS_REPO}/tests/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
 
 ulimit -s unlimited
 mpiexec -n ${TASKS} ./fv3.exe >out 2> >(tee err >&3)
